9bf3d7b00a85a904e219722fdd62e983fc0930d4
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-04-25 09:01:27 +0300

    event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    // create a variable and set the handleFocus function to print out Good
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+// create a variable and set the handleBlur function to print out Hey Eyes on me 
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    //set the onFocus and onBlur events to call the functions 
+    <button
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,30 @@
 // Code Keypad Component Here
 
+
+
 function Keypad (){
+    //handleSubmit 
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(` ${event.target.name} : ${ event.target.value}.`)
+
+    }
+    //handleChange
+    function handleChange(event) {
+        console.log('Entering password...')
+
+    }
     return (
-        <div></div>
+        <>
+        <form onSubmit={handleSubmit} >
+            <input type= "password"
+                   name= "password"
+                   onChange={handleChange}
+                   placeholder="Enter password"
+                   />
+            <button>Login</button>
+        </form>
+        </>
     )
 }
 
